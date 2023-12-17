a0e471d42901efd3c4f9514060958a98c68d4422
chhong <chhong@donga.com>
2023-12-17 11:56:10 +0900

    Initial commit

---
+++ src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApplication.class, args);
+    }
+
+}
