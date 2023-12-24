7d832d56f6fa0695df40a0e9e0506a3804d3cc82
chhong <chhong@donga.com>
2023-12-24 15:04:26 +0900

    simple push test

--- src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
@@ -8,7 +8,7 @@ public class JenkinsTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsTestApplication.class, args);
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < 13; i++) {
             System.out.println(i);
         }
     }
