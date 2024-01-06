9749489fc2630ade116085907b65bc3c858f6943
chhong <chhong@donga.com>
2024-01-06 14:40:27 +0900

    simple push test

--- src/main/java/com/example/jenkinstest/controller/TestController.java
+++ src/main/java/com/example/jenkinstest/controller/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+
     @GetMapping(value = "/",produces = "application/json")
     public String myResponse() {
         return "success";
