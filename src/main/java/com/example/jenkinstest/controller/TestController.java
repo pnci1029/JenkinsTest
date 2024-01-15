997f19e1896647da74dc82011e53a8ea592e4b6d
chhong <pnci1029@gmail.com>
2024-01-15 15:21:56 +0900

    branch test

--- src/main/java/com/example/jenkinstest/controller/TestController.java
+++ src/main/java/com/example/jenkinstest/controller/TestController.java
@@ -11,7 +11,7 @@ public class TestController {
 
     @GetMapping(value = "/",produces = "application/json")
     public String myResponse() {
-        return "success14";
+        return "success15";
 //        return new MyResponse(1L, "hello");
     }
 
